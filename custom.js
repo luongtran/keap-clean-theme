0c4abd246e4a0bd09d3be07c14324f141c86c4be
luongtran <luongtran@users.noreply.github.com>
2026-04-23 00:47:49 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,26 +1,20 @@
+<script>
 window.addEventListener("load", function(){
 
-    // wait until everything fully rendered
-    setTimeout(function(){
+ setTimeout(function(){
 
-        const checkout = document.querySelector("form");
+   let btns = [...document.querySelectorAll("button,div,a")];
 
-        if(!checkout) return;
+   let cc = btns.find(el =>
+      el.innerText &&
+      el.innerText.toLowerCase().includes("credit card")
+   );
 
-        // add wrapper class
-        checkout.classList.add("keap-fix");
+   if(cc){
+      cc.click();
+   }
 
-        // change only submit button text
-        const btn = checkout.querySelector('input[type="submit"],button[type="submit"]');
-
-        if(btn){
-            if(btn.tagName==="INPUT"){
-                btn.value="Complete Order";
-            }else{
-                btn.innerText="Complete Order";
-            }
-        }
-
-    }, 2500); // wait Keap scripts finish render
+ },2000);
 
 });
+</script>

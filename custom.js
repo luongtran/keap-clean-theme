cefa6aba005a29cd8d7653ce30d7a144c2fad9b2
luongtran <luongtran@users.noreply.github.com>
2026-04-23 09:11:41 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,36 +1,13 @@
-window.addEventListener("load", function(){
+let t = setInterval(()=>{
 
-    function autoOpenCard(){
+ let btn = [...document.querySelectorAll("*")].find(el =>
+   el.innerText &&
+   el.innerText.includes("Pay with Credit Card")
+ );
 
-        let all = [...document.querySelectorAll("button, div, span, a")];
+ if(btn){
+   btn.click();
+   clearInterval(t);
+ }
 
-        let btn = all.find(el =>
-            el.innerText &&
-            el.innerText.trim().toLowerCase().includes("pay with credit card")
-        );
-
-        if(btn){
-            btn.click();
-            console.log("Opened Credit Card");
-        }
-    }
-
-    // thử nhiều lần vì keap render chậm
-    let tries = 0;
-
-    let timer = setInterval(function(){
-
-        autoOpenCard();
-
-        tries++;
-
-        // nếu thấy card field rồi thì stop
-        let card = document.body.innerText.toLowerCase().includes("card number");
-
-        if(card || tries > 10){
-            clearInterval(timer);
-        }
-
-    }, 1000);
-
-});
+},1000);
